61db44c9fe7ca79da12194807cac522d654cbd44
root <root@ip-172-31-50-251.ec2.internal>
2019-01-09 09:21:27 +0000

    Python Q&A

---
+++ allv.2.py
@@ -0,0 +1,36 @@
+#!usr/bin/python
+
+
+import sys, os, subprocess
+#from random import shuffle
+import re
+import random
+
+#print os.mkdir('/root/python-work/ppp')
+#print "The new directory has been created successfully"
+
+
+
+#print "The below code is just printing the list"
+#list = [2, 3, 5, 7, 4, 10]
+#for i in list:
+#    print i
+
+#print "The below code is converting strings into ASCII values"
+#string = 'z'
+#print "ASCII value of a is: %s" %ord(string)
+
+#l = []
+#for i in range(100,200):
+#    if (i%7==0) and (i%5!=0):
+#        l.append(i)
+#        print l
+
+
+print "The bwlo code is which accepts a sequence of comma-separated numbers from console and generate a list"
+#list = [34,67,55,33,12,98]
+l1 = []
+for i in list:
+    i = raw_input("Enter the numbers: ")        
+    l1.append(l1)
+    print l1.split(',')
